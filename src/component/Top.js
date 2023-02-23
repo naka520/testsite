ffc12ce93f024edac44b439227e51b70b05d581f
naka520 <yashipipi0520@gmail.com>
2023-02-24 02:34:57 +0900

    "Add TopPageLayout"

--- src/component/Top.js
+++ src/component/Top.js
@@ -1,11 +1,18 @@
 import Style from "../App.css";
 import logo from "./logo.svg";
 import Header from "./Header";
+import { Stack, Typography } from "@mui/material";
+import { Box, width } from "@mui/system";
 
 const Top = () => {
   return (
     <div className={Style.App}>
       <Header />
+      <Stack>
+        <Box sx={{ width: 200, height: 200 }}>
+          <Typography>カレンダー</Typography>
+        </Box>
+      </Stack>
     </div>
   );
 };
