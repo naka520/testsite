488a5a4dc4c910e3bf2c701866c0fab0e45d2981
naka520 <yashipipi0520@gmail.com>
2023-02-24 03:25:44 +0900

    "Add Layout calender"

--- src/component/Top.js
+++ src/component/Top.js
@@ -3,14 +3,17 @@ import logo from "./logo.svg";
 import Header from "./Header";
 import { Stack, Typography } from "@mui/material";
 import { Box, width } from "@mui/system";
+import Calender from "./Calender";
 
 const Top = () => {
   return (
     <div className={Style.App}>
       <Header />
       <Stack>
-        <Box sx={{ width: 200, height: 200 }}>
-          <Typography>カレンダー</Typography>
+        <Box sx={{ width: 400, height: 400 }}>
+          <Typography>
+            <Calender />
+          </Typography>
         </Box>
       </Stack>
     </div>
